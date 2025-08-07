55812a669a594476a11a4f3f1c673e6c878f636f
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2025-08-07 11:27:13 +0000

    project creation

---
+++ bingo-ideas/src/pages/TileGenerator.jsx
@@ -0,0 +1,12 @@
+import bosses from 'common/json/bosses.json';
+
+const TileGenerator = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TileGenerator;
